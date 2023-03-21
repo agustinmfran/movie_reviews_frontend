f2fc22215c53171ed6090d0eb9c460daa6893dc1
agustinmfran <agustinmfran@gmail.com>
2023-03-21 18:30:03 +0100

    pulido

--- src/services/movies.js
+++ src/services/movies.js
@@ -2,35 +2,50 @@ import axios from "axios";
 
 class MovieDataService {
   getAll(page = 0) {
-    return axios.get(`http://localhost:5000/api/v1/movies?page=${page}`);
+    return axios.get(
+      `https://movies-reviews-backend.vercel.app/api/v1/movies?page=${page}`
+    );
   }
 
   get(id) {
-    return axios.get(`http://localhost:5000/api/v1/movies/id/${id}`);
+    return axios.get(
+      `https://movies-reviews-backend.vercel.app/api/v1/movies/id/${id}`
+    );
   }
 
   find(query, by = "title", page = 0) {
     return axios.get(
-      `http://localhost:5000/api/v1/movies?${by}=${query}&page=${page}`
+      `https://movies-reviews-backend.vercel.app/api/v1/movies?${by}=${query}&page=${page}`
     );
   }
 
   createComment(data) {
-    return axios.post("http://localhost:5000/api/v1/movies/comments", data);
+    return axios.post(
+      "hhttps://movies-reviews-backend.vercel.app/api/v1/movies/comments",
+      data
+    );
   }
 
   updateComment(data) {
-    return axios.put("http://localhost:5000/api/v1/movies/comments", data);
+    return axios.put(
+      "https://movies-reviews-backend.vercel.app/api/v1/movies/comments",
+      data
+    );
   }
 
   deleteComment(id, userId) {
-    return axios.delete("http://localhost:5000/api/v1/movies/comments", {
-      data: { comment_id: id, user_id: userId },
-    });
+    return axios.delete(
+      "https://movies-reviews-backend.vercel.app/api/v1/movies/comments",
+      {
+        data: { comment_id: id, user_id: userId },
+      }
+    );
   }
 
   getRatings() {
-    return axios.get("http://localhost:5000/api/v1/movies/ratings");
+    return axios.get(
+      "https://movies-reviews-backend.vercel.app/api/v1/movies/ratings"
+    );
   }
 }
 
